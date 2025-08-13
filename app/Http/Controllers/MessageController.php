cd6d275b96fa28fd5e062e0b050bf2bac92f1307
Dimitri Zindovic <dimitri.zindovic2@gmail.com>
2025-08-13 15:57:39 +0200

    Adds message creation endpoint

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\MessageSent;
 use App\Http\Controllers\Controller;
+use App\Models\Group;
 use App\Models\Message;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
@@ -16,5 +18,21 @@ class MessageController extends Controller
             ->get();
 
         return response()->json($messages);
+    }
+
+    public function store(Request $request, Group $group): JsonResponse
+    {
+        $validated = $request->validate([
+            'content' => 'required|string|max:1000',
+        ]);
+
+        $message = new Message($validated);
+        $message->group()->associate($group);
+        $message->sender()->associate(auth()->user());
+        $message->save();
+
+        broadcast(new MessageSent($message));
+
+        return response()->json($message, 201);
     }
 }
