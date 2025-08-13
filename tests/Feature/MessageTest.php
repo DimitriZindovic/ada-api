ca5c436191cc050451790d29c064220a23bf5a46
Dimitri Zindovic <dimitri.zindovic2@gmail.com>
2025-08-13 17:03:25 +0200

    Adds message sending test

---
+++ tests/Feature/MessageTest.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Models\User;
+use App\Models\Group;
+use App\Events\MessageSent;
+use Illuminate\Support\Facades\Event;
+
+beforeEach(function () {
+    $this->user = User::first();
+    $this->actingAs($this->user);
+
+    // Désactiver les événements de broadcasting pendant les tests
+    Event::fake();
+});
+
+test('user can send message to group', function () {
+    $group = Group::create([
+        'name' => 'Test Group',
+        'description' => 'Group for testing messages',
+        'creator_id' => $this->user->id,
+    ]);
+
+    $group->users()->attach($this->user->id);
+
+    $messageData = [
+        'content' => 'Hello, this is a test message!',
+    ];
+
+    $response = $this->postJson("/api/message/{$group->id}", $messageData);
+
+    $response->assertStatus(201);
+
+    $this->assertDatabaseHas('messages', [
+        'content' => $messageData['content'],
+        'group_id' => $group->id,
+        'sender_id' => $this->user->id,
+    ]);
+
+    // Vérifier que l'événement MessageSent a été déclenché
+    Event::assertDispatched(MessageSent::class);
+});
