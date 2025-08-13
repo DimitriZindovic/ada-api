d5464e74d171ab2c907ed946977ef619ba681a67
Dimitri Zindovic <dimitri.zindovic2@gmail.com>
2025-08-13 12:03:42 +0200

    Updates allowed origins for CORS

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://ada-front-sb.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
