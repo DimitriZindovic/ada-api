0bdc012fab45923f86ab8b34d747e85f7c0cfc52
Dimitri Zindovic <dimitri.zindovic2@gmail.com>
2025-08-13 15:57:22 +0200

    Replaces UUID with autoincrementing ID

--- database/migrations/2025_08_13_000003_create_group_user_table.php
+++ database/migrations/2025_08_13_000003_create_group_user_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('group_user', function (Blueprint $table) {
-            $table->uuid('id')->primary();
+            $table->id();
             $table->foreignId('group_id')->constrained('groups')->cascadeOnDelete()->cascadeOnUpdate();
             $table->foreignId('user_id')->constrained('users')->cascadeOnDelete()->cascadeOnUpdate();
             $table->timestamp('joined_at')->useCurrent();
