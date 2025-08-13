afb4e095c8e4f23be88637169cee48770eddc9a6
Dimitri Zindovic <dimitri.zindovic2@gmail.com>
2025-08-13 11:29:25 +0200

    Updates allowed origins for CORS

--- config/cors.php
+++ config/cors.php
@@ -21,9 +21,7 @@ return [
 
     'allowed_origins' => [
         'http://localhost:5173',
-        'http://localhost:3000',
-        'http://127.0.0.1:3000',
-        // 'render',
+        'https://ada-front-ten.vercel.app'
     ],
 
     'allowed_origins_patterns' => [],
