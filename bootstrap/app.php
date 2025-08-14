11ed028929603ee49e74279fe473088ecdc40aeb
Dimitri Zindovic <dimitri.zindovic2@gmail.com>
2025-08-14 10:46:52 +0200

    Enables CORS support for API

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,22 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // Middleware pour gérer automatiquement les requêtes OPTIONS
+        $middleware->api(prepend: [
+            function ($request, $next) {
+                if ($request->getMethod() === 'OPTIONS') {
+                    return response('', 200)
+                        ->header('Access-Control-Allow-Origin', '*')
+                        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+                        ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With')
+                        ->header('Access-Control-Max-Age', '86400');
+                }
+                
+                $response = $next($request);
+                $response->headers->set('Access-Control-Allow-Origin', '*');
+                return $response;
+            }
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
